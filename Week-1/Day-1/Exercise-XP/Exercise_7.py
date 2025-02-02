399fa269939474e84329a842933d117d86331c4e
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-02 14:53:02 +0200

    added exercise 9

--- Week-1/Day-1/Exercise-XP/Exercise_7.py
+++ Week-1/Day-1/Exercise-XP/Exercise_7.py
@@ -1,3 +1,5 @@
+# Write code that asks the user for a number and determines whether this number is odd or even.
+
 user_number = input("Type a number: ")
 if int(user_number)%2 == 0:
     print(f"{user_number} is an even number.")
--- Week-1/Day-1/Exercise-XP/Exercise_8.py
+++ Week-1/Day-1/Exercise-XP/Exercise_8.py
@@ -1,3 +1,5 @@
+# Write code that asks the user for their name and determines whether or not you have the same name, print out a funny message based on the outcome.
+
 my_name = "gabriel"
 user_name = input("Type your name: ").lower()
 if user_name == my_name:
