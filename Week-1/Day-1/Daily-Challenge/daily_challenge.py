7cbcdf8ca9b13ddc10ca263511fea8e347eca1f4
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-02 15:50:01 +0200

    added items 3 and 4 to daiy challenge

--- Week-1/Day-1/Daily-Challenge/daily_challenge.py
+++ Week-1/Day-1/Daily-Challenge/daily_challenge.py
@@ -1,3 +1,5 @@
+import random
+
 some_name = input("Please type a string of exactly 10 characters: ")
 length_of_name = len(some_name)
 
@@ -8,12 +10,19 @@ elif length_of_name > 10:
 else:
     print("perfect string")
 
+print()
+
 print(some_name[0])
 print(some_name[-1])
+
+print()
 
 partial_name = ""
 for n in range(0, length_of_name):
     partial_name += some_name[n]
     print(partial_name)
 
+print()
 
+name_shuffled = ''.join(random.sample(some_name, length_of_name))
+print(f"Shuffled name: {name_shuffled}\n")
