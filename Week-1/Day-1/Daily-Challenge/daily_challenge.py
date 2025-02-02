68f749539d7f1a32ea707e547576b877c723989e
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-02 15:20:51 +0200

    added items 1 and 2 of daily challenge

---
+++ Week-1/Day-1/Daily-Challenge/daily_challenge.py
@@ -0,0 +1,12 @@
+some_name = input("Please type a string of exactly 10 characters: ")
+length_of_name = len(some_name)
+
+if length_of_name < 10:
+    print("string not long enough")
+elif length_of_name > 10:
+    print("string too long")
+else:
+    print("perfect string")
+
+print(some_name[0])
+print(some_name[-1])
