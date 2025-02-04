2985edd6e7ac350716073959144b193e4004104e
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-04 13:29:47 +0200

    added exercise 1

---
+++ Week-1/Day-3/Exercise-XP/exercise_1.py
@@ -0,0 +1,9 @@
+# Convert the two following lists into dictionaries.
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dict_names_ages = dict(zip(keys, values))
+print(dict_names_ages)
