d5a110b12985b14c8018aa81b20ade2ad0b5d0ad
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-05 14:14:53 +0200

    added exercise 3

---
+++ Week-1/Day-4/Exercise-XP/exercise_3.py
@@ -0,0 +1,5 @@
+def describe_city(city, country="The United States of America (a.k.a. 'Merica)"):
+    print(f"{city} in {country}.")
+
+describe_city("Jerusalem", "Israel")
+describe_city("Maryland")
