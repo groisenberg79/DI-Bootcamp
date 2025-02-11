efefb06ed44487379c9be4d318152d0140d46710
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-11 03:54:01 +0200

    add daily challenge

---
+++ Week-2/Day-2/Daily-Challenge/daily_challenge.py
@@ -0,0 +1,55 @@
+import math
+
+class Pagination:
+    def __init__(self, items=None, pageSize=10):
+        self.items = items
+        self.pageSize = int(pageSize)
+        self.totalPages = math.ceil(len(items)/pageSize)
+        self.currentPage = 1
+
+    def getVisibleItems(self):
+        firstIndex = (self.currentPage - 1) * self.pageSize
+        if self.currentPage == self.totalPages:
+            page_items = self.items[firstIndex:]
+            print(page_items)
+        else:
+            lastIndex = firstIndex + self.pageSize 
+            page_items = self.items[firstIndex: lastIndex]
+            print(page_items)
+
+    def firstPage(self):
+        self.currentPage = 1
+
+    def nextPage(self):
+        if self.currentPage < self.totalPages:
+            self.currentPage += 1
+        else:
+            pass
+    
+    def prevPage(self):
+        if self.currentPage > 1:
+            self.currentPage -= 1
+        else:
+            pass
+    
+    def lastPage(self):
+        self.currentPage = self.totalPages
+    
+    def goToPage(self, pageNum):
+        if int(pageNum) > self.totalPages:
+            self.currentPage = self.totalPages
+        elif int(pageNum) <= 0:
+            self.currentPage = 1
+        else:
+            self.currentPage = int(pageNum)
+
+# >>>>>>> TESTING THE CODE
+
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+
+p = Pagination(alphabetList, 4.2)
+p.getVisibleItems() 
+p.nextPage()
+p.getVisibleItems()
+p.lastPage()
+p.getVisibleItems()
