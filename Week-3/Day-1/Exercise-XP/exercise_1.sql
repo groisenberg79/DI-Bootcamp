e95b88257689220b63ce45cc66d897933dc774af
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-16 14:07:12 +0200

    add exercise 1

---
+++ Week-3/Day-1/Exercise-XP/exercise_1.sql
@@ -0,0 +1,41 @@
+-- Item 1 of exercise:
+
+-- CREATE TABLE items (
+-- item_type VARCHAR(50),
+-- price INTEGER
+-- )
+
+-- CREATE TABLE customers (
+-- first_name VARCHAR(50),
+-- last_name VARCHAR(50)
+-- )
+
+-- Item 2 of exercise:
+
+-- INSERT INTO items (item_type, price)
+-- VALUES
+-- ('Small Desk', 100),
+-- ('Large desk', 300),
+-- ('Fan', 80)
+
+-- INSERT INTO items (first_name, last_name)
+-- VALUES
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson')
+
+-- item of exercise:
+
+-- SELECT * FROM items
+
+-- SELECT * FROM items WHERE price > 80
+
+-- SELECT * FROM items WHERE price <= 300
+
+-- SELECT * FROM customers WHERE last_name = 'Smith'
+
+-- SELECT * FROM customers WHERE last_name = 'Jones'
+
+-- SELECT * FROM customers WHERE first_name != 'Scott'
