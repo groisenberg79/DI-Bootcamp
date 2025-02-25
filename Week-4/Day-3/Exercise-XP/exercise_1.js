b67014733ba4a214dc943bc38d4f4e27eb51a6a9
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-02-25 18:48:40 +0200

    add exercise 1

--- Week-4/Day-3/Exercise-XP/exercise_1.js
+++ Week-4/Day-3/Exercise-XP/exercise_1.js
@@ -1,14 +1,14 @@
-function displayNumbersDivisible() {
+function displayNumbersDivisible(divisor) {
     let sum = 0;
     let num_str = '';
     for (let i = 0; i <= 500; i++){
-        if (i % 23 == 0){
+        if (i % divisor == 0){
             num_str += i + ' '
             sum += i
         }
     }
-    console.log('numbers divisible by 23:', num_str)
+    console.log('numbers divisible by '+  divisor + ':', num_str)
     console.log('sum: ', sum)
 }
 
-displayNumbersDivisible()
+displayNumbersDivisible(23)
