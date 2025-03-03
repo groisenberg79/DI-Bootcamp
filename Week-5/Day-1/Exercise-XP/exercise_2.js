df33e4993c6d41b00f0e5916ab68d37706768035
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-03-03 15:41:51 +0200

    add exercise 2

---
+++ Week-5/Day-1/Exercise-XP/exercise_2.js
@@ -0,0 +1,17 @@
+// function winBattle(){
+//     return true;
+// }
+
+// item 1
+const winBattle = () => {
+    return true;
+}
+
+// item 2
+let experiencePoints;
+
+// item 3
+if (winBattle ?  experiencePoints = 10 : experiencePoints = 1);
+
+// item 4
+console.log(experiencePoints);
