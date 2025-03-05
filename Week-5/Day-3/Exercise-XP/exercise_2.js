9db7b3ccca0a736b16e5d7b2fd374977c7b050dc
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-03-05 18:21:23 +0200

    add exercise 2

---
+++ Week-5/Day-3/Exercise-XP/exercise_2.js
@@ -0,0 +1,7 @@
+
+function displayStudentInfo(objUser){
+    const { first, last} = objUser;
+    console.log(`Your full name is ${first} ${last}.`)
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
