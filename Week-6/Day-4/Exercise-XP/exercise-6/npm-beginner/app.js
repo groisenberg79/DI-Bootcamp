db367bd7d3dd2821a9a5ff542e85cd1486ae5f76
Gabriel Roisenberg Rodrigues <groisenberg79@gmail.com>
2025-03-17 11:35:41 +0200

    add exercise 6

---
+++ Week-6/Day-4/Exercise-XP/exercise-6/npm-beginner/app.js
@@ -0,0 +1,10 @@
+// n app.js, require the chalk package and use it to color and style text in the terminal.
+
+// Write a simple script that uses chalk to print a colorful message.
+
+// Run app.js using Node.js and observe the colorful output in the terminal.
+import chalk from "chalk";
+
+console.log(chalk.blue("This is blue. "));
+console.log(chalk.red("This is red. "));
+console.log(chalk.magenta("This is magenta. "));
