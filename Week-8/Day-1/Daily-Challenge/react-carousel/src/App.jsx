f7cbff07bfbab5f0d72a20468fea07f2d447ce58
gabriel <groisenberg79@gmail.com>
2025-03-30 01:32:28 +0300

    add daily challenge

---
+++ Week-8/Day-1/Daily-Challenge/react-carousel/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import hongKong from './assets/hong-kong.jpg';
+import macau from './assets/macau.webp';
+import japan from './assets/japan.webp';
+import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // required CSS
+import { Carousel } from 'react-responsive-carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel width="500px">
+        <div>
+          <img src={hongKong} alt="Slide 1" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src={macau} alt="Slide 2" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src={japan} alt="Slide 3" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default App;
